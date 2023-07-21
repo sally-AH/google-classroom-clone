75ee9b9d354373cbd6803f741ed5d629e971752f
ToniTannoury <tatannoury01@gmail.com>
2023-07-22 02:03:17 +0300

    Adjusted icon location

--- landingPage.js
+++ landingPage.js
@@ -156,7 +156,7 @@ classes.forEach((studentClass, index) => {
         </span>
         <a href="/c/NjE1MzI3NzQ1ODkx/sp/NjE1NTAwOTkzOTIy/all" aria-label="Open your work for &quot;FSW 23&amp; 24 | Tech&quot;" data-tooltip-enabled="true" data-tooltip-id="zxtDBb615327745891" jsaction=""></a>
       </div>
-      <div>
+      <div class="bottomIcons">
         <span>
           <svg focusable="false" width="24" height="24" viewBox="0 0 24 24">
             <path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"></path>
