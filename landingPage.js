90badb1ff7664e7a34b82f4d5422922ac84302b9
ToniTannoury <tatannoury01@gmail.com>
2023-07-22 01:00:23 +0300

    Initiated landingPage.js with a dummy array of classes

---
+++ landingPage.js
@@ -0,0 +1,119 @@
+
+
+classes = [
+  
+  {
+     id:1,
+     name:"FSW23&24 | Tech",
+     instructor:'Christopher Yammine',
+     image:'./home.png',
+
+     description:"full stack web dev bootcamp tech department",
+     color:"black",
+     assignments:[
+       {
+         name:"Google Classroom Clone",
+         dueDate:"10-12-2023T11.00PM"
+       }
+     ]
+   },
+   {
+     id:2,
+     color:"green",
+     name:"FSW23&24 | Soft Skills",
+     instructor:'George Abed el Nour',
+     image:'./3177440.png',
+     description:"full stack web dev bootcamp soft skills department",
+     assignments:[
+       {
+         name:"pro dev stuff",
+         dueDate:"9-12-2023T10.00PM"
+       }
+     ]
+   },
+   {
+     id:2,
+     color:"green",
+     name:"FSW23&24 | Soft Skills",
+     instructor:'George Abed el Nour',
+     image:'./3177440.png',
+     description:"full stack web dev bootcamp soft skills department",
+     assignments:[
+       {
+         name:"pro dev stuff",
+         dueDate:"9-12-2023T10.00PM"
+       }
+     ]
+   },
+   {
+     id:2,
+     color:"green",
+     name:"FSW23&24 | Soft Skills",
+     instructor:'George Abed el Nour',
+     image:'./3177440.png',
+     description:"full stack web dev bootcamp soft skills department",
+     assignments:[
+       {
+         name:"pro dev stuff",
+         dueDate:"9-12-2023T10.00PM"
+       }
+     ]
+   },
+   {
+     id:2,
+     color:"green",
+     name:"FSW23&24 | Soft Skills",
+     instructor:'George Abed el Nour',
+     image:'./3177440.png',
+     description:"full stack web dev bootcamp soft skills department",
+     assignments:[
+       {
+         name:"pro dev stuff",
+         dueDate:"9-12-2023T10.00PM"
+       }
+     ]
+   },
+   {
+     id:2,
+     color:"green",
+     name:"FSW23&24 | Soft Skills",
+     instructor:'George Abed el Nour',
+     image:'./3177440.png',
+     description:"full stack web dev bootcamp soft skills department",
+     assignments:[
+       {
+         name:"pro dev stuff",
+         dueDate:"9-12-2023T10.00PM"
+       }
+     ]
+   },
+   {
+     id:2,
+     color:"green",
+     name:"FSW23&24 | Soft Skills",
+     instructor:'George Abed el Nour',
+     image:'./3177440.png',
+     description:"full stack web dev bootcamp soft skills department",
+     assignments:[
+       {
+         name:"pro dev stuff",
+         dueDate:"9-12-2023T10.00PM"
+       }
+     ]
+   },
+   {
+     id:2,
+     color:"green",
+     name:"FSW23&24 | Soft Skills",
+     instructor:'George Abed el Nour',
+     image:'./3177440.png',
+     description:"full stack web dev bootcamp soft skills department",
+     assignments:[
+       {
+         name:"pro dev stuff",
+         dueDate:"9-12-2023T10.00PM"
+       }
+     ]
+   },
+ 
+]
