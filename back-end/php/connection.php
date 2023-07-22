a64278b271ae7f888e957f6e8cb457198d87df7b
Charbel Allawi <charbelallawiwork@gmail.com>
2023-07-22 19:19:02 +0300

    sign in php added

--- back-end/php/connection.php
+++ back-end/php/connection.php
@@ -1,9 +1,10 @@
+
 <?php
 
 $host = "localhost";
 $db_user = "root";
 $db_pass = null;
-$db_name = "foodex_db";
+$db_name = "classroom_db";
 
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 if (!$mysqli) {
