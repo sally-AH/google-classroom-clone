d2c09e8417cda1dfad95177f22eb9a7be86adc97
ToniTannoury <tatannoury01@gmail.com>
2023-07-22 22:43:27 +0300

    Added Dummy class Work

---
+++ front-end/javascript/classworkPage.js
@@ -0,0 +1,46 @@
+posts = [
+  {
+        id: 1,
+        post_type: 'material',
+        category:"Full Stack",
+        title: "Fetch/PHP Assignment Correction",
+        instructo: 'Khaled Faour',
+        posted_date: 'Yesterday',
+  },
+
+  {
+        id: 2,
+        post_type: 'assignment',
+        category:"PHP",
+        name:" Google Classroom Clone",
+        due_date: "Due 15 Dec, 10:00 PM",
+  },
+  {
+        id: 3,
+        post_type: 'material',
+        category:"PHP",
+        name:"Log in Sign Up",
+        due_date: "Due 12 Dec, 6:00 AM",
+  },
+  {
+        id: 4,
+        post_type: 'assignment',
+        category:"SQL",
+        name:"Tables and relations",
+        due_date: "Due 11 Jan, 11:00 PM",
+  },
+  {
+        id: 5,
+        post_type: 'material',
+        category:"JavaScript",
+        name:"DOM Manipulation",
+        due_date: "Due 26 Jul, 10:00 PM",
+  },
+  {
+        id: 6,
+        post_type: 'assignment',
+        category:"UI/UX Design",
+        name:"Color Theory",
+        due_date: "Due 07 Oct, 1:00 AM",
+  }
+]
