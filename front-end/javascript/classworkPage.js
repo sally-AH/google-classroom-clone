045419415e14bb83964af74db267b2df79b5d7fa
ToniTannoury <tatannoury01@gmail.com>
2023-07-23 03:05:49 +0300

    Displayeddynamically posts with dummy data

--- front-end/javascript/classworkPage.js
+++ front-end/javascript/classworkPage.js
@@ -45,15 +45,69 @@ posts = [
   }
 ]
 
+const categories = []
+
 document.addEventListener("DOMContentLoaded", () => {
   const ulElement = document.querySelector('.aside ul');
-  // Check if the 'ul' element was found
-  for(i  in posts){
-    console.log(posts[i].category)
+  const categories = [];
+
+  for (let i in posts) {
+    categories.push(posts[i].category);
     if (ulElement) {
-      ulElement.innerHTML+=`<li class="topic">${posts[i].category}</li>`
-      // Add your code to interact with the 'ul' element here
+      ulElement.innerHTML += `<li class="topic">${posts[i].category}</li>`;
     }
   }
-  
-})
+
+  for (let i = 0; i < categories.length; i++) {
+    const joinedName = categories[i].trim().replace(/\s/g, '');
+    document.querySelector(".categories").innerHTML += `
+      <li class="category ${joinedName}">
+        <div>
+          <h1 class="title">${categories[i]}</h1>
+          <div class="line cat">1</div>
+        </div>
+      </li>`;
+  }
+
+  for (let i = 0; i < posts.length; i++) {
+    const joinedName = posts[i].category.trim().replace(/\s/g, '');
+    const elementsWithCategory = document.querySelectorAll(`[class*="${joinedName}"]`)
+    for(let j=0 ; j<elementsWithCategory.length ; j++){
+      elementsWithCategory[j].innerHTML+=`
+      <ul  class="assMat">
+          <li>
+            <div>
+              <div class="singlePost">
+                ${posts[i].post_type === "assignment"? `<div class="logoContainer">
+                <svg
+                focusable="false"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                class="assignmentsLogo"
+              >
+                <path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z"></path>
+              </svg>
+              </div>` :`<div class="logoContainer2">
+              <svg
+              focusable="false"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              class="assignmentsLogo"
+            >
+              <path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z"></path>
+            </svg>
+            </div>` }
+              </div>
+            </div>
+          </li>
+        </ul>`
+    }
+    console.log(elementsWithCategory);
+    
+  }
+});
+
+
+{/*  */}
