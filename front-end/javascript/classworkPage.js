7ad7b082b56d383bc7706eb15d03d220f9180520
ToniTannoury <tatannoury01@gmail.com>
2023-07-23 03:58:14 +0300

    Displayedposts with no category on top

--- front-end/javascript/classworkPage.js
+++ front-end/javascript/classworkPage.js
@@ -2,6 +2,7 @@ posts = [
   {
         id: 1,
         post_type: 'material',
+        name:" full stuff from stuff",
         category:"Full Stack",
         title: "Fetch/PHP Assignment Correction",
         instructo: 'Khaled Faour',
@@ -42,6 +43,13 @@ posts = [
         category:"UI/UX Design",
         name:"Color Theory",
         due_date: "Due 07 Oct, 1:00 AM",
+  },
+  {
+        id: 7,
+        post_type: 'material',
+        category:"",
+        name:"Color Theory",
+        due_date: "Due 07 Oct, 1:00 AM",
   }
 ]
 
@@ -50,12 +58,16 @@ const categories = []
 document.addEventListener("DOMContentLoaded", () => {
   const ulElement = document.querySelector('.aside ul');
   const categories = [];
+  
 
   for (let i in posts) {
-    categories.push(posts[i].category);
-    if (ulElement) {
-      ulElement.innerHTML += `<li class="topic">${posts[i].category}</li>`;
+    if(!categories.includes(posts[i].category)){
+      categories.push(posts[i].category);
+      if (ulElement) {
+        ulElement.innerHTML += `<li class="topic">${posts[i].category}</li>`;
+      }
     }
+    
   }
 
   for (let i = 0; i < categories.length; i++) {
@@ -89,24 +101,61 @@ document.addEventListener("DOMContentLoaded", () => {
                 <path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z"></path>
               </svg>
               </div>` :`<div class="logoContainer2">
-              <svg
-              focusable="false"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              class="assignmentsLogo"
-            >
-              <path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z"></path>
-            </svg>
+              <svg class="logoMat" focusable="false" width="24" height="24" viewBox="0 0 24 24" class=" NMm5M hhikbc"><path d="M7 15h7v2H7zm0-4h10v2H7zm0-4h10v2H7z"></path><path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-.14 0-.27.01-.4.04a2.008 2.008 0 0 0-1.44 1.19c-.1.23-.16.49-.16.77v14c0 .27.06.54.16.78s.25.45.43.64c.27.27.62.47 1.01.55.13.02.26.03.4.03h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7-.25c.41 0 .75.34.75.75s-.34.75-.75.75-.75-.34-.75-.75.34-.75.75-.75zM19 19H5V5h14v14z"></path></svg>
+            
+                   
             </div>` }
+                <div>
+                  <h4 class="assMat text">${posts[i].name}</h4>
+                </div>
               </div>
             </div>
           </li>
         </ul>`
     }
     console.log(elementsWithCategory);
-    
+    if (!posts[i].category) {
+      const noCategUl = document.querySelector('.noCateg ul');
+      if (noCategUl) {
+        noCategUl.innerHTML += `
+        <li>
+              <div class="">
+                <div class="singlePost">
+                  <div>
+                  ${posts[i].post_type === "assignment"? `<div class="logoContainer">
+                  <svg
+                  focusable="false"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  class="assignmentsLogo"
+                >
+                  <path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z"></path>
+                </svg>
+                </div>
+                <div class="line cat">1</div>` :`<div class="logoContainer2" >
+                <svg class="logoMat"  focusable="false" width="24" height="24" viewBox="0 0 24 24" class=" NMm5M hhikbc"><path d="M7 15h7v2H7zm0-4h10v2H7zm0-4h10v2H7z"></path><path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-.14 0-.27.01-.4.04a2.008 2.008 0 0 0-1.44 1.19c-.1.23-.16.49-.16.77v14c0 .27.06.54.16.78s.25.45.43.64c.27.27.62.47 1.01.55.13.02.26.03.4.03h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7-.25c.41 0 .75.34.75.75s-.34.75-.75.75-.75-.34-.75-.75.34-.75.75-.75zM19 19H5V5h14v14z"></path></svg>
+              
+                     
+              </div>` }
+              
+                  </div>
+                  <div>
+                    <h4 class="assMat text">Full Stack stuff</h4>
+                  </div>
+                  
+                </div>
+                
+              </div>
+              
+            </li>
+            
+            `;
+      }
+      
+    }
   }
+  
 });
 
 
