69a4eeeba270bb203fc4740a07d594da68c31417
Charbel Allawi <charbelallawiwork@gmail.com>
2023-07-23 16:58:44 +0300

    PHPMailer Added

---
+++ back-end/php/send.php
@@ -0,0 +1,54 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Origin");
+$response = array();
+$json_data = file_get_contents('php://input');
+$data = json_decode($json_data, true);
+$receivedemail = $data['email'];
+//Declare variable
+// $email = $_POST[$receivedemail];
+// $subject = $_POST["subject"];
+// $message = $_POST["message"];
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+//password generator
+
+try {
+    //Server settings
+    $mail->isSMTP();                      //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com'; //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;             //Enable SMTP authentication
+    $mail->Username   = 'sefactoryassignment@gmail.com'; //SMTP username
+    $mail->Password   = 'ijufctnhasvegpej'; //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+    $mail->Port       = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('sefactoryassignment@gmail.com', 'Group Team');
+    $mail->addAddress($receivedemail , 'user'); //Add a recipient
+
+    //Content
+    $mail->isHTML(true); //Set email format to HTML
+    $mail->Subject = "this is a test email";
+    $mail->Body    = "your new password is : DoFAJmasa@891";
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
