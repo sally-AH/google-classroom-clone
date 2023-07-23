b479551b4e52497606a2c735d0ee3b6c9295634a
RamaHabbal <137204591+RamaHabbal@users.noreply.github.com>
2023-07-24 02:30:43 +0300

    Updated people to be dynamic

---
+++ front-end/javascript/people.js
@@ -0,0 +1,62 @@
+const people = [
+    {
+      id: 1,
+      teacherName: "Teacher name",
+      teacherImage: "/front-end/images/3177440.png",
+      role: "teacher",
+    },
+    {
+      id: 2,
+      studentName: "Student name",
+      studentImage: "/front-end/images/3177440.png",
+      role: "student",
+    },
+    {
+      id: 3,
+      teacherName: "Another teacher",
+      teacherImage: "/front-end/images/3177440.png",
+      role: "teacher",
+    },
+    {
+      id: 4,
+      studentName: "Another student",
+      studentImage: "/front-end/images/3177440.png",
+      role: "student",
+    },
+  ];
+  
+  const teacherUsers = [];
+  const studentUsers = [];
+  
+  people.forEach((element) => {
+    const { id, teacherName, teacherImage, role } = element;
+    const { id: studentId, studentName, studentImage } = element;
+  
+    if (role === "teacher") {
+      teacherUsers.push(`
+        <div class="container1" id="${id}">
+          <img src="${teacherImage}" alt="Teacher image">
+          <div>
+            <p class="TeacherName">${teacherName}</p>
+          </div>
+        </div>
+      `);
+    } else if (role === "student") {
+      studentUsers.push(`
+        <div class="container2" id="${studentId}">
+          <img src="${studentImage}" alt="Student image">
+          <div>
+            <p class="studentName">${studentName}</p>
+          </div>
+        </div>
+      `);
+    }
+  });
+  
+  
+  const teacherSection = document.querySelector(".upper_part");
+  teacherSection.innerHTML = teacherUsers.join("");
+  
+  const studentSection = document.querySelector(".down_part");
+  studentSection.innerHTML = studentUsers.join("");
+  
