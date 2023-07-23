993c60ef6bd493c6534a3f125f229fcdf09f1ba4
Charbel Allawi <charbelallawiwork@gmail.com>
2023-07-23 16:57:52 +0300

    password generator function added

--- front-end/javascript/forgotpass.js
+++ front-end/javascript/forgotpass.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   let forgotemail = document.getElementById("forgotemail")
+  let newpassword = ""
 
   document.querySelector(".forgotbtn").addEventListener("click", async (e) => {
 
-
+    function passwordgenerator() {
+      var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var passwordLength = 12;
+      var password = "";
+      for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+      }
+      return password
+    }
+    newpassword = passwordgenerator()
 
     jsdata = {
       email: forgotemail.value,
-
+      password: newpassword
     }
     const jsonobject = JSON.stringify(jsdata)
     const default_url = "https://localhost/google-classroom-clone/back-end/php/send.php"
