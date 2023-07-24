bf3ff256a32c57a3cc8354d2a93cf17a71e86770
majed <majedhabli2020@gmail.com>
2023-07-24 15:21:04 +0300

    updated the stream html, css, and Updated the button navigation function.

--- front-end/javascript/stream.js
+++ front-end/javascript/stream.js
@@ -1,9 +1,17 @@
 // const search = window.location.search
 
 // console.log(search.split('=')[1])
+const stream_route = document.getElementById("stream_route");
 const classwork_route = document.getElementById('classwork_route');
+const people_route = document.getElementById('people_route');
 classwork_route.addEventListener("click",gotToPage);
+people_route.addEventListener("click",gotToPage);
 
-function gotToPage(){
-    window.location.href= "/front-end/html/classwork.html";
+const line = document.getElementById("line_people");
+const line1 = document.getElementById("line_classwork");
+    line.style.display  = 'none';
+    line1.style.display = 'none';
+
+function gotToPage(event){
+  window.location.href= `/front-end/html/${event.target.className}.html`;
 }
