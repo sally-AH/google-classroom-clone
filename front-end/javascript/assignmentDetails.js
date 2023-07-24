5f917b5e60698a8bca0ed0f340534349f6c8b628
majed <majedhabli2020@gmail.com>
2023-07-25 02:38:30 +0300

    Updated the js files for the new module in page assignment.

--- front-end/javascript/assignmentDetails.js
+++ front-end/javascript/assignmentDetails.js
@@ -2,57 +2,32 @@ let btnClicked = false;
 let showModal = false;
 
 window.addEventListener('load',function(){
-  const add_symbol= document.getElementById("addSymbol");
-  add_symbol.addEventListener("click", function(){
+  const uploadbtn= document.getElementById("uploadbtn");
+  uploadbtn.addEventListener("click", function(){
     btnClicked = !btnClicked;
     checkToggle();
     console.log(btnClicked);
   });
 
-  const create_class = document.getElementById("createClassBtn");
-  create_class.addEventListener("click",function(){
-    showModal = !showModal;
-    checkModalToggle();
-  });
-
-  const cancel = document.getElementById("cancel");
-  cancel.addEventListener("click",function(){
+  const close = document.getElementById("closebtn");
+  close.addEventListener("click",function(){
     hideModal();
-  });
-
-  const create = document.getElementById("create");
-  cancel.addEventListener("click",function(){
-    createClass();
   });
 
 })
 
 function checkToggle(){
   if (btnClicked == true){
-    const popup_module=document.getElementById("popup");
-    popup_module.style.display = "block";
+    const popup_module=document.getElementById("upload_files_popup");
+    popup_module.style.display = "flex";
   }else{
-    const popup_module=document.getElementById("popup");
+    const popup_module=document.getElementById("upload_files_popup");
     popup_module.style.display = "none";
-  }
-}
-
-function checkModalToggle(){
-  if (showModal == true){
-    const module=document.getElementById("create_class_popup");
-    module.style.display = "flex";
-  }else{
-    const module=document.getElementById("create_class_popup");
-    module.style.display = "none";
   }
 }
 
 function hideModal(){
   showModal == false
-  const module=document.getElementById("create_class_popup");
+  const module=document.getElementById("upload_files_popup");
     module.style.display = "none";
-}
-
-function createClass(){
-  // do somthin
 }
