79ebe34b8d5555bd251ee863d294e19a29d48135
majed <majedhabli2020@gmail.com>
2023-07-24 12:19:58 +0300

    updated the stream html, css, and Added the button navigation function.

--- front-end/javascript/stream.js
+++ front-end/javascript/stream.js
@@ -1,3 +1,9 @@
-const search = window.location.search
+// const search = window.location.search
 
-console.log(search.split('=')[1])
+// console.log(search.split('=')[1])
+const classwork_route = document.getElementById('classwork_route');
+classwork_route.addEventListener("click",gotToPage);
+
+function gotToPage(){
+    window.location.href= "/front-end/html/classwork.html";
+}
